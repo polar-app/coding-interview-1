ed8cbb7be23778711d86668c074723310dd0e803
Kevin Burton <burtonator@gmail.com>
2021-07-22 17:11:53 -0500

    ...

---
+++ src/NGramProfiles.ts
@@ -0,0 +1,34 @@
+import {NGramParser} from "./NGramParser";
+
+export namespace NGramProfiles {
+
+    import NGramStr = NGramParser.NGramStr;
+
+    export interface NGramProfile {
+        [key: string]: number
+    }
+
+    /**
+     * The NGram rank including the text of the ngram, and it's count.
+     */
+    export interface INGramRank {
+        readonly text: NGramStr;
+        readonly count: number;
+    }
+
+    /**
+     * Give some text, and an n-gram length, compute a score of the n-grams and
+     * then return the top N grams as a result.
+     *
+     * IMPORTANT: given text for a language, what's the maximum amount of space
+     * that would be stored for n-gram limits?
+     *
+     * @param text The text for computing ngrams.
+     * @param len The len of the ngrams.
+     * @param limit the max number of ranked ngrams to return.
+     */
+    export function compute(text: string, len: number, limit: number): ReadonlyArray<INGramRank> {
+        return [];
+    }
+
+}
